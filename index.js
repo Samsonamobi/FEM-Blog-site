f5aa775dca29366450583ec6b53e53713d79392d
Samson Amobi <samsonamobi66@gmail.com>
2024-07-19 03:23:50 +0100

    added hamburger menu

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const hamburgerToggle = document.querySelector("#hamburger-toggle");
-const navLinks = document.querySelector("#navLinks");
+let open = document.getElementById("open"),
+    close = document.getElementById("close"),
+    navLinks = document.getElementById("navLinks");
 
-hamburgerToggle.addEventListener("click", ()=>{
-    if (!navLinks.classList.contains("open")) {
-        navLinks.classList.add("open")
-    }else{
-        navLinks.classList.remove("open");
+
+
+    Slider = (x,y,z) => {
+        x.addEventListener("click", ()=>{
+            x.classList.toggle("hide");
+            y.classList.toggle("hide");
+
+            if(z== "slide-in"){
+                navLinks.classList.toggle("slide")
+            }
+            if(z== "slide-out"){
+                navLinks.classList.toggle("slide")
+            }
+        })
     }
-})
+
+    Slider(open,close, "slide-in")
+    Slider(close,open, "slide-out")
+
+    // open.addEventListener("click", ()=>{
+    //     open.classList.toggle("hide");
+    //     close.classList.toggle("hide");
+    // })
+
+    // close.addEventListener("click", ()=>{
+    //     open.classList.toggle("hide");
+    //     close.classList.toggle("hide");
+    // })
